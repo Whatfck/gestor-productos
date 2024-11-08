740f81242b80d456573e1ddd0a7feac46eb75b59
Whatfck <76444868+Whatfck@users.noreply.github.com>
2024-11-08 22:02:41 +0000

    123

--- gestor/gestor_app/forms.py
+++ gestor/gestor_app/forms.py
@@ -4,4 +4,4 @@ from .models import Producto
 class ProductoForm(forms.ModelForm):
     class Meta:
         model = Producto
-        fields = ['nombre', 'descripcion', 'precio', 'cantidad', 'disponibilidad']
+        fields = ['nombre', 'referencia', 'descripcion', 'precio', 'cantidad', 'disponibilidad']
--- gestor/gestor_app/models.py
+++ gestor/gestor_app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Producto(models.Model):
     nombre = models.CharField(max_length=100)
+    referencia = models.CharField(max_length=100)
     descripcion = models.TextField()
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     cantidad = models.IntegerField()  # Cambiado de stock a cantidad
